4840394f97f51c839fbf6af5b75904692cce0c6f
boom <nuntipatcpe@gmail.com>
2022-09-16 02:18:39 +0700

    feat : Futures market success

---
+++ src/Constants.ts
@@ -0,0 +1,7 @@
+export const BALANCE = "BALANCE";
+export const RISK_OF_RUIN = "RISK_OF_RUIN";
+export const STOP_LOSS = "STOP_LOSS";
+export const LAVERAGE = "LAVERAGE";
+export const ROUND = "ROUND";
+export const RISK_REWARD_RATIO = "RISK_REWARD_RATIO";
+export const WIN_RATE = "WIN_RATE";
--- src/components/pages/Futures/Futures.tsx
+++ src/components/pages/Futures/Futures.tsx
@@ -1,19 +1,27 @@
-import { useEffect, useState } from "react";
-
+import { useState } from "react";
+import { comma } from "number-magic";
 import {
-  comma,
-  toFixed,
-  percentage,
-  idxList,
-  randomInt,
-  capital,
-  precise,
-  isNumber,
-} from "number-magic";
+  BALANCE,
+  LAVERAGE,
+  RISK_OF_RUIN,
+  RISK_REWARD_RATIO,
+  ROUND,
+  STOP_LOSS,
+  WIN_RATE,
+} from "../../../Constants";
 
 type FuturesProps = {};
 
-type Input = {
+type Int = {
+  balance: number;
+  ror: number;
+  sl: number;
+  lr: number;
+  round: number;
+  rr: number;
+  winrate: number;
+};
+type Str = {
   balance: string;
   ror: string;
   sl: string;
@@ -24,56 +32,47 @@ type Input = {
 };
 
 const Futures: React.FC<any> = () => {
-  const [input, setInput] = useState<Input>({
-    balance: "10,000",
-    ror: "4",
-    sl: "3",
+  const [input, setInput] = useState<Str>({
+    balance: "1,000",
+    ror: "3",
+    sl: "10",
     lr: "5",
-    round: "0",
+    round: "33",
     rr: "5",
     winrate: "60",
   });
-
-  useEffect(() => {
-    console.log(calculatr().maxRound);
-    // console.log(calculatr().positionSize);
-  }, [input]);
+  const [valueSlider, setValueSlider] = useState<string>(
+    comma(calculatr().maxRound)
+  );
 
   function calculatr() {
-    console.log(parseFloat(input.ror.replaceAll(",", "")));
-
-    let balance = parseFloat(input.balance.replaceAll(",", ""));
-    let ror = parseFloat(input.ror.replaceAll(",", "")) / 100;
-    let sl = parseFloat(input.sl.replaceAll(",", "")) / 100;
-    let lr = parseFloat(input.lr.replaceAll(",", ""));
-    let round = parseInt(input.round.replaceAll(",", ""));
-    let rr = parseFloat(input.rr.replaceAll(",", ""));
-    let winrate = parseFloat(input.winrate.replaceAll(",", "")) / 100;
-    // console.log(ror);
-
-    let bXror = balance * ror;
-    let slXlr = sl * lr;
-    let positionSize = bXror / slXlr;
-
-    let x = balance * ror;
-    let maxRound = Math.floor(balance / x);
-
-    let lossRate = 100 - winrate * 100;
-
-    let roundProfit = Math.floor(winrate * round);
-
-    let profit = Math.round(sl * lr * rr * positionSize * roundProfit);
-
-    let roundLoss = Math.floor((lossRate / 100) * round);
-
-    let loss = Math.round(sl * lr * positionSize * roundLoss);
-
-    let summary = profit - loss;
-
-    let profitPercen = (summary / balance) * 100;
+    let conToInt: Int = {
+      balance: parseFloat(input.balance.replaceAll(",", "")),
+      ror: parseFloat(input.ror.replaceAll(",", "")) / 100,
+      sl: parseFloat(input.sl.replaceAll(",", "")) / 100,
+      lr: parseFloat(input.lr.replaceAll(",", "")),
+      round: parseInt(input.round.replaceAll(",", "")),
+      rr: parseFloat(input.rr.replaceAll(",", "")),
+      winrate: parseFloat(input.winrate.replaceAll(",", "")) / 100,
+    };
+    let positionSize: number =
+      (conToInt.balance * conToInt.ror) / (conToInt.sl * conToInt.lr);
+    let lossRate: number = 100 - conToInt.winrate * 100;
+    let roundProfit: number = Math.floor(conToInt.winrate * conToInt.round);
+    let profit: number = Math.round(
+      conToInt.sl * conToInt.lr * conToInt.rr * positionSize * roundProfit
+    );
+    let roundLoss: number = Math.floor((lossRate / 100) * conToInt.round);
+    let loss: number = Math.round(
+      conToInt.sl * conToInt.lr * positionSize * roundLoss
+    );
+    let summary: number = profit - loss;
+    let profitPercen: number = (summary / conToInt.balance) * 100;
     return {
       positionSize,
-      maxRound,
+      maxRound: Math.floor(
+        conToInt.balance / (conToInt.balance * conToInt.ror)
+      ),
       lossRate,
       summary,
       profitPercen,
@@ -83,191 +82,168 @@ const Futures: React.FC<any> = () => {
       roundLoss,
     };
   }
-
-  function onChengNumber(e: any, value: string) {
-    let numb = e.target.value;
-    let valueConToSting: string = numb.replaceAll(",", "");
-
+  function onChangeNumberFormath(e: string, value: string) {
+    let numb = e.replaceAll(",", "");
     if (
       numb.charAt(numb.length - 1) === "." ||
       numb.charAt(numb.length - 1) === "0"
     ) {
-      updateCasePoint(value, comma(valueConToSting));
+      updateInput(value, comma(numb));
     } else {
-      updateCasePoint(
-        value,
-
-        comma(parseFloat(valueConToSting))
-      );
-    }
-    if (e.target.value === "") {
-      updateCasePoint(value, "0");
+      updateInput(value, comma(parseFloat(numb)));
     }
   }
-
-  function calLosstRate() {
-    let winrate = parseInt(input.winrate.replaceAll(",", ""));
-    let sum = 100 - winrate;
-
-    return sum;
-  }
-
-  function updateCasePoint(value: string, check: string) {
+  function updateInput(value: string, newValue: string) {
     switch (value) {
-      case "balance":
-        setInput({ ...input, balance: check });
+      case BALANCE:
+        setInput({ ...input, balance: newValue });
         break;
-      case "ror":
-        setInput({ ...input, ror: check });
+      case RISK_OF_RUIN:
+        setInput({ ...input, ror: newValue });
         break;
-      case "sl":
-        setInput({ ...input, sl: check });
+      case STOP_LOSS:
+        setInput({ ...input, sl: newValue });
         break;
-      case "lr":
-        setInput({ ...input, lr: check });
+      case LAVERAGE:
+        setInput({ ...input, lr: newValue });
         break;
-      case "round":
-        setInput({ ...input, round: check });
+      case ROUND:
+        setInput({ ...input, round: newValue });
         break;
-      case "rr":
-        setInput({ ...input, rr: check });
+      case RISK_REWARD_RATIO:
+        setInput({ ...input, rr: newValue });
         break;
-      case "winrate":
-        setInput({ ...input, winrate: check });
+      case WIN_RATE:
+        setInput({ ...input, winrate: newValue });
         break;
       default:
         break;
     }
   }
+
   return (
     <div className="futures">
       <div className="title">
         <span style={{ color: "#950101" }}>Futures</span>
         <span> market</span>
       </div>
+      {/* box1___________________________________________________ */}
       <form action="" className="risk-of-ruin">
         <label htmlFor="">Balance</label>
         <input
           type="text"
-          onChange={(e) => onChengNumber(e, "balance")}
+          onChange={(e) => onChangeNumberFormath(e.target.value, BALANCE)}
           value={input.balance === "NaN" ? "0" : input.balance}
-          // defaultValue={"0"}
         />
-        {/* <InputNumberCommas defaultValue={1} /> */}
         <label htmlFor="">USD</label>
-
         <label htmlFor="">Risk of Ruin</label>
-        {/* <input type="number" defaultValue={10000} /> */}
         <input
           type="text"
-          onChange={(e) => onChengNumber(e, "ror")}
+          onChange={(e) => onChangeNumberFormath(e.target.value, RISK_OF_RUIN)}
           value={input.ror === "NaN" ? "0" : input.ror}
-          // defaultValue={"0"}
         />
         <label htmlFor="">%</label>
-
         <label htmlFor="">Stop loss</label>
-        {/* <input type="number" defaultValue={10000} /> */}
         <input
           type="text"
-          onChange={(e) => onChengNumber(e, "sl")}
+          onChange={(e) => onChangeNumberFormath(e.target.value, STOP_LOSS)}
           value={input.sl === "NaN" ? "0" : input.sl}
-          // defaultValue={"0"}
         />
         <label htmlFor="">%</label>
-
         <label htmlFor="">Laverage</label>
         <input
           type="text"
-          onChange={(e) => onChengNumber(e, "lr")}
+          onChange={(e) => onChangeNumberFormath(e.target.value, LAVERAGE)}
           value={input.lr === "NaN" ? "0" : input.lr}
-          // defaultValue={"0"}
         />
         <label htmlFor="">X</label>
       </form>
+      {/* box2___________________________________________________ */}
       <form action="">
         <label htmlFor="">Position size</label>
         <label htmlFor="">{comma(calculatr().positionSize)}</label>
         <label htmlFor="">USD</label>
-
         <label htmlFor="">Max round</label>
         <label htmlFor="">{comma(calculatr().maxRound)}</label>
         <label htmlFor="">*</label>
       </form>
+      {/* box3___________________________________________________ */}
       <form action="">
         <label htmlFor="">Round</label>
         <div>
           <div className="cal-round container-winrate">
-            <span>0</span>
-            {/* <input type="number" defaultValue={calMaxRound()} /> */}
+            <span></span>
             <input
               type="text"
-              onChange={(e) => onChengNumber(e, "round")}
+              onChange={(e) => onChangeNumberFormath(e.target.value, ROUND)}
               value={input.round === "NaN" ? "0" : input.round}
-              // defaultValue={"0"}
             />
+          </div>
+          <div className="slider-con">
+            <span>0</span>
+            <input
+              type="range"
+              min="0"
+              max={calculatr().maxRound}
+              value={valueSlider}
+              className="slider"
+              onChange={(e) => {
+                setValueSlider(e.target.value);
+                setInput({ ...input, round: e.target.value });
+              }}
+            ></input>
             <span>{calculatr().maxRound}</span>
           </div>
         </div>
         <label htmlFor="">*</label>
-
         <label htmlFor="">Risk Reward Ratio</label>
-
         <div className="risk-reward-ratio container-winrate">
           <span>1 :</span>
-          {/* <input type="number" defaultValue={"10,000"} /> */}
           <input
             type="text"
-            onChange={(e) => onChengNumber(e, "rr")}
+            onChange={(e) =>
+              onChangeNumberFormath(e.target.value, RISK_REWARD_RATIO)
+            }
             value={input.rr === "NaN" ? "0" : input.rr}
-            // defaultValue={"0"}
           />
-          <span></span>
         </div>
         <label htmlFor="">*</label>
-
         <label htmlFor="">Win rate</label>
         <div className={"container-winrate"}>
           <div></div>
-          {/* <input type="number" defaultValue={"60"} /> */}
-
           <input
             type="text"
-            onChange={(e) => onChengNumber(e, "winrate")}
+            onChange={(e) => onChangeNumberFormath(e.target.value, WIN_RATE)}
             value={input.winrate === "NaN" ? "0" : input.winrate}
-            // defaultValue={"0"}
           />
-          <div></div>
         </div>
         <label htmlFor="">%</label>
       </form>
+      {/* card___________________________________________________ */}
       <div className="card">
         <p>Loss rate</p>
         <span>{calculatr().lossRate}</span>
         <p>%</p>
-
         <p>Summary</p>
         <span>{comma(calculatr().summary)}</span>
         <p>USD</p>
-
         <p>Profit</p>
         <span>{comma(calculatr().profitPercen)}</span>
         <p>%</p>
       </div>
+      {/* card profit-Loss____________________________________________ */}
       <div className="profit-Loss">
         <div className="card">
           <p>Profit</p>
           <p>{comma(calculatr().profit)}</p>
-          <p>usd</p>
-
+          <p>USD</p>
           <p>Round</p>
           <p>{calculatr().roundProfit}</p>
         </div>
         <div className="card">
           <p>Loss</p>
           <p>{comma(calculatr().loss)}</p>
-          <p>usd</p>
-
+          <p>USD</p>
           <p>Round</p>
           <p>{calculatr().roundLoss}</p>
         </div>
@@ -275,5 +251,4 @@ const Futures: React.FC<any> = () => {
     </div>
   );
 };
-
 export default Futures;
